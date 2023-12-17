2daf1ef530f1feb4788595a4c0899a5fba0d0c25
single-byte <joharkhan1999@gmail.com>
2023-12-17 19:23:41 +0500

    Add axios dependency and update App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/todos").then((response) => {
+      setTodos(response.data);
+    });
+  }, []);
+
+  const addTodo = () => {
+    if (task.trim() !== "") {
+      axios
+        .post("http://localhost:5000/api/todos", { task })
+        .then((response) => {
+          setTodos([...todos, response.data]);
+          setTask("");
+        });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ToDo List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Task</button>
+      </div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo._id}>{todo.task}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect("mongodb://localhost:27017/tododb", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
